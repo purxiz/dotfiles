b914468d62f2f46f1e79f4258a2a473049d38597
purxiz <functionofn@gmail.com>
2020-04-28 16:28:20 -0400

    fixin up rofi config

--- .config/fish/functions/edit_config.fish
+++ .config/fish/functions/edit_config.fish
@@ -1,8 +1,10 @@
+#!/usr/bin/fish
+
 function edit_config
-	set program (cat $HOME/.config/scripts/list | cut -d' ' -f1 | rofi -dmenu -p "Config:")
+	set program (cat "$HOME"/.config/scripts/list | cut -d' ' -f1 | rofi -dmenu -p "Config:")
 	echo "$program"
 	if test -n "$program"
-		set file (cat $HOME/.config/scripts/list | grep -w "$program" | cut -d' ' -f2)
-		exec x-terminal-emulator -e "vim $file"
+		set file (cat "$HOME"/.config/scripts/list | grep -w "$program" | cut -d' ' -f2)
+		x-terminal-emulator -e "vim $file;"
 	end
 end
