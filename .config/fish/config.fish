3df55de363faf02947fdfb38a01df3a11afbc390
purxiz <functionofn@gmail.com>
2020-04-28 13:00:20 -0400

    fisher package manager
    Use fisher to track plugins for the fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,19 @@
+# enable vi style key-bindings in the terminal
 fish_vi_key_bindings
+
+# custom git alias for use in home directory to avoid colliding with "real" repos
 function config
 	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv
 end
 
+# make sure vim is used for general editor stuff
+set -x VISUAL vim
+set -x EDITOR $VISUAL
+
+# happy greetings if fortune is installed
+function fish_greeting
+	if not type fortune > /dev/null 2>&1
+	else
+		fortune
+	end
+end
