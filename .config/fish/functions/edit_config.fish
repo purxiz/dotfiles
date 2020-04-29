6262079c28e5994a938433e8bf1eef0f7766e9a3
purxiz <functionofn@gmail.com>
2020-04-29 14:17:41 -0400

    nord theme for ROFI
    A nord theme for rofi didn't exist yet, so I made one myself. Relevant
    config files changed to obey :)

--- .config/fish/functions/edit_config.fish
+++ .config/fish/functions/edit_config.fish
@@ -1,10 +1,10 @@
-#!/usr/bin/fish
+#!/usr/bin/env fish
 
 function edit_config
 	set program (cat "$HOME"/.config/scripts/list | cut -d' ' -f1 | rofi -dmenu -p "Config:")
 	echo "$program"
 	if test -n "$program"
-		set file (cat "$HOME"/.config/scripts/list | grep -w "$program" | cut -d' ' -f2)
-		x-terminal-emulator -e "vim $file;"
+		set file (cat "$HOME"/.config/scripts/list | grep -w "$program" | cut -d' ' -f2 | sed "s|~|$HOME|g")
+		x-terminal-emulator -- vim "$file"
 	end
 end
