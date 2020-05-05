137749a110e0ef14b5fd352739517d5d427324da
purxiz <functionofn@gmail.com>
2020-05-05 11:50:01 -0400

    notes bugfix
    Displays full path of notes properly in rofi menu

--- .config/fish/functions/edit_notes.fish
+++ .config/fish/functions/edit_notes.fish
@@ -1,6 +1,6 @@
 function edit_notes
 	set NOTES_DIR $HOME/notes
-	set NOTE (ls -1 $NOTES_DIR | rofi -dmenu)
+	set NOTE (find $NOTES_DIR -type f | sed -e "s|$NOTES_DIR/||g" | rofi -dmenu)
 	if test -n "$NOTE"
 		mkdir -p (dirname $NOTES_DIR/$NOTE)
 		x-terminal-emulator -- kak "$NOTES_DIR/$NOTE"
