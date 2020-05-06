289378372535f82f484079eabf6c5401b5b9cb72
purxiz <functionofn@gmail.com>
2020-05-06 15:29:41 -0400

    rgrep notes
    Added the ability to grep through the notes directory by opening the notes menu with $mod+n, and then typing 'find <search_string>'
    It supports whatever regex the basic grep command supports

--- .config/fish/functions/edit_file.fish
+++ .config/fish/functions/edit_file.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 function edit_file
-	set program (cat "$HOME"/.config/scripts/list | cut -d' ' -f1 | rofi -dmenu -p "Config:")
+	set program (cat "$HOME"/.config/scripts/list | cut -d' ' -f1 | rofi -dmenu -p "Config")
 	echo "$program"
 	if test -n "$program"
 		set file (cat "$HOME"/.config/scripts/list | grep -w "$program" | cut -d' ' -f2 | sed "s|~|$HOME|g" | head -n 1)
--- .config/fish/functions/edit_notes.fish
+++ .config/fish/functions/edit_notes.fish
@@ -1,7 +1,11 @@
 function edit_notes
 	set NOTES_DIR $HOME/notes
-	set NOTE (find $NOTES_DIR -type f | sed -e "s|$NOTES_DIR/||g" | rofi -dmenu)
-	if test -n "$NOTE"
+	set NOTE (find $NOTES_DIR -type f | sed -e "s|$NOTES_DIR/||g" | rofi -dmenu -p "Notes")
+	if test (echo $NOTE | cut -d' ' -f1) = 'find'
+		set FILE (grep -r (echo $NOTE | cut -d' ' -f1 --complement) $NOTES_DIR | sed -e "s|$NOTES_DIR/||g" | rofi -dmenu -p "Notes")
+		set NOTE (echo "$FILE" | cut -d':' -f1 | sed -e "s|$NOTES_DIR||g")
+		x-terminal-emulator -- kak "$NOTES_DIR/$NOTE"
+	else if test -n "$NOTE"
 		mkdir -p (dirname $NOTES_DIR/$NOTE)
 		x-terminal-emulator -- kak "$NOTES_DIR/$NOTE"
 	end
